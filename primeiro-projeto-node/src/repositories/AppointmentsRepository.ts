e9d85b1107c66c4388af946bcd294436ba649873
V1n1c1us <v.f.diehl@gmail.com>
2020-04-13 22:36:55 -0300

    List all appointments

--- primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -8,6 +8,10 @@ class AppointmentRespository {
     this.appointments = [];
   }
 
+  public getAll(): Appointment[] {
+    return this.appointments;
+  }
+
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find(appointment =>
       isEqual(date, appointment.date),
