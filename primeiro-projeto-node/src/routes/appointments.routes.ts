1366cfef4e29e3e9d05aae8c98876109a993bac7
V1n1c1us <v.f.diehl@gmail.com>
2020-04-13 22:47:38 -0300

    Applying the concept of DTO and the Omit helper

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -26,7 +26,10 @@ appoitmentsRouter.post('/', (request, response) => {
       .json({ message: 'This Appointments is already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
